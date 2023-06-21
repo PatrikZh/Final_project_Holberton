4b15c5acb1e1b92f6b2164973cc40e3f72120dc7
PatrikZh <pzheji@actirana.edu.al>
2023-06-21 20:31:59 +0200

    Update

--- database.py
+++ database.py
@@ -4,4 +4,9 @@ import sqlite3
 def connect_to_database():
     sql = sqlite3.connect('crudapplication.db')
     sql.row_factory = sqlite3.Row
-    return sql 
+    return sql
+
+def get_database():
+    if not hasattr(g, 'crudapplication_db'):
+        g.crudapplication_db = connect_to_database()
+    return g.crudapplication.db
